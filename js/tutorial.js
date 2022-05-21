045b61b732e02803d8eb5f1a68f87c9ed1ea8767
MalyshViktoriia <102228181+MalyshViktoriia@users.noreply.github.com>
2022-05-21 15:28:12 +0200

    add html and js files

---
+++ js/tutorial.js
@@ -0,0 +1,49 @@
+// const myName = "Mango";
+// console.log("Name - ", myName);
+// const sum = 300;
+// console.log("Sum - ", sum);
+
+// const type = 700;
+// console.log(typeof type);
+
+// // alert("Hello!");
+
+// // const shouldRenew = confirm("Хочете продовжити підписку?");
+// // console.log(shouldRenew);
+
+// let quantity = prompt("Wright the amount");
+// quantity = Number(quantity);
+// console.log(quantity);
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+
+// let salary = 1300.20123;
+// salary = Number(salary.toFixed(1));
+// // salary = Number(salary);
+
+// console.log(salary);
+
+// Возведение в степень
+// const result = Math.pow(2, 3);
+// console.log(result);
+
+
+// let base = prompt("Введите число");
+// base = Number(base);
+// console.log(base);
+
+// let power = prompt("Введите степень");
+// power = Number(power);
+// console.log(power);
+
+// let result = base ** power;
+// console.log(result);
+const max = 50;
+const min = 20;
+
+const result = Math.round(Math.random() * (max - min) + min);
+
+console.log(result);
+
+// Math.random() * (max - min) + min
